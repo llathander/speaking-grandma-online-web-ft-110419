571d6155c2ca8fce8374a5ba282846e9c4f2ea23
Adrian Samaniego <llathander@gmail.com>
2019-11-09 23:34:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,16 +1,11 @@
-#def speak_to_grandma(phrase)
-#  if phrase != phrase.upcase()
-#    return "HUH?! SPEAK UP, SONNY!"
-#  elsif phrase == phrase.upcase()
-#    return "NO, NOT SINCE 1938!"
-#  else phrase == "I LOVE YOU GRANDMA!"
-#    puts "I LOVE YOU TOO PUMPKIN!"
-#  end
-#end
-
-
 def speak_to_grandma(phrase)
-  if phrase = "I LOVE YOU TOO GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase()
+    return "NO, NOT SINCE 1938!"
+  else phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  end
 end
-end
+
+
